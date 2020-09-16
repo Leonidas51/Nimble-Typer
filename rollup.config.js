86a2ff7b7b19c265209303f4692279de71955761
Leonidas51 <kirpichik61@gmail.com>
2020-09-16 15:34:08 +0300

    pretty much done

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,17 @@
-import json from '@rollup/plugin-json';
 import {terser} from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss'
 import typescript from 'rollup-plugin-typescript2';
 
-export default {
+export default [{
   input: 'src/index.ts',
   output: {
     file: './dist/index.min.js',
     format: 'iife',
     plugins: [ terser() ]
   },
-  plugins: [ json(), typescript(), postcss() ]
-};
+  plugins: [
+    typescript(),
+    postcss()
+  ]
+  }
+];
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import words from "./words.json";
+import words from "./words.js";
 
 interface Word {
   text: string;
@@ -20,8 +20,12 @@ class Game {
   input: HTMLInputElement;
   overlay: HTMLElement;
   play_area: HTMLElement;
-  new_word_chance: number = 0.1;
+  msg_container: HTMLElement;
+  new_word_chance_base: number = 0.1;
+  new_word_chance: number = this.new_word_chance_base;
   words_entered: number = 0;
+  longest_correct: string = "";
+  longest_error: string = "";
   speed: number = 0.3;
   ticks: number = 1;
 
@@ -29,33 +33,69 @@ class Game {
     this.input = <HTMLInputElement>document.getElementById("main-input");
     this.play_area = document.getElementById("play-area");
     this.overlay = document.getElementById("overlay");
+    this.msg_container = document.getElementById("msg-container");
   }
 
   init(): void {
     this.tick = this.tick.bind(this);
+    this.startGame = this.startGame.bind(this);
     this.input.value = "";
 
-    document.addEventListener("keyup", this.enterWord.bind(this));
-    document.getElementById("btn-init").addEventListener("click", this.startGame.bind(this));
+    window.addEventListener("keyup", this.enterWord.bind(this));
+    document.getElementById("btn-init").addEventListener("click", this.startGame);
+    document.getElementById("btn-restart").addEventListener("click", this.startGame);
   }
 
   startGame(): void {
     this.overlay.style.display = "none";
+    this.overlay.querySelector(<any>"#overlay-start").style.display = "none";
+    this.overlay.querySelector(<any>"#overlay-endgame").style.display = "none";
+
     this.input.removeAttribute("disabled");
     this.input.focus();
+    this.input.value = "";
+    
+    this.new_word_chance_base = 0.1;
+    this.new_word_chance = this.new_word_chance_base;
+    this.words_entered = 0;
+    this.longest_correct = "";
+    this.longest_error = "";
+    this.speed = 0.3;
+    this.ticks = 1;
 
     this.addWord();
     this.tick();
+
+    this.showMessage("Вводите слова, пока они не дошли до правого края!");
   }
  
   addWord(): void {
     const new_word = this.getRandomWord();
     const new_el = document.createElement("span")
+    let new_word_top
 
     new_el.className = "word";
     new_el.style.right = "800px";
-    new_el.style.top = Math.round(Math.random() * 380) + "px";
     new_el.innerHTML = new_word;
+
+    //take 5 tries to not let words overlap
+    for (let i = 0; i < 5; i++) {
+      let fail = false;
+      new_word_top = Math.round(Math.random() * 380);
+  
+      this.current_words.forEach(word => {
+        let top = parseInt(word.el.style.top)
+        if (Math.abs(new_word_top - top) <= 15) {
+          fail = true;
+        }
+      })
+
+      if (!fail) {
+        break;
+      }
+    }
+
+    new_el.style.top = new_word_top + "px";
 
     this.play_area.appendChild(new_el);
     this.current_words.push({
@@ -74,27 +114,67 @@ class Game {
     this.current_words.forEach((word, i) => {
       if (this.input.value === word.text) {
         
-        word.el.remove();
+        this.animateWordRemoval(word.el);
         this.current_words.splice(i, 1);
       }
     })
 
-    if (prev_len > this.current_words.length) {
+    if (prev_len > this.current_words.length) { // word entered correctly
+      if (this.input.value.length > this.longest_correct.length) {
+        this.longest_correct = this.input.value;
+      }
+
       this.input.value = "";
       this.words_entered++;
 
-      if (this.words_entered && this.words_entered % 5 === 0) {
-        this.speed += 0.1
+      if ((this.words_entered + 5) % 10 === 0) { // 5, 15, 25...
+        this.speed += 0.1;
+        this.showMessage('Скорость увеличена!');
+      } else if (this.words_entered && this.words_entered % 10 === 0) { // 10, 20, 30...
+        this.new_word_chance_base += 0.1;
+        this.showMessage('Больше слов!');
       }
+    } else { // error in word
+      if (this.input.value.length > this.longest_error.length) {
+        this.longest_error = this.input.value;
+      }
+
+      this.input.classList.add("error");
+
+      setTimeout(() => {
+        this.input.classList.remove("error");
+      }, 700)
     }
+  }
+
+  animateWordRemoval(el): void {
+    el.classList.add("remove-animate");
+
+    setTimeout(() => {
+      el.remove();
+    }, 1000);
+  }
+
+  showMessage(msg): void {
+    this.msg_container.innerHTML = msg;
+    this.msg_container.style.opacity = "1";
+
+    setTimeout(() => {
+      this.msg_container.style.opacity = "0";
+    }, 1000);
   }
 
   gameOver(): void {
     this.current_words = [];
-
     this.input.setAttribute("disabled", "");
-
     this.play_area.innerHTML = "";
+    
+    this.overlay.style.display = "block";
+    this.overlay.querySelector(<any>"#overlay-endgame").style.display = "block";
+
+    document.getElementById("endgame-value-score").innerHTML = String(this.words_entered);
+    document.getElementById("endgame-value-longest-correct").innerHTML = this.longest_correct || "N/A";
+    document.getElementById("endgame-value-longest-error").innerHTML = this.longest_error || "N/A";
   }
 
   getRandomWord(): string {
@@ -114,7 +194,7 @@ class Game {
 
     if (randomChance(this.new_word_chance)) {
       this.addWord();
-      this.new_word_chance = 0.1;
+      this.new_word_chance = this.new_word_chance_base;
     } else {
       this.new_word_chance += 0.001;
     }
